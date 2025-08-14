e86fcf6a019cb0c211d92bda2cd98e8edc7c8cdc
andruAJ <giraldoandresjuan0405@gmail.com>
2025-08-14 14:59:09 -0500

    Arreglo de ids

--- RestAPI/Assets/Scripts/HTTP_Test.cs
+++ RestAPI/Assets/Scripts/HTTP_Test.cs
@@ -8,24 +8,29 @@ using UnityEngine.UIElements;
 public class HTTP_Test : MonoBehaviour
 {
     [SerializeField]
-    private int characterId = 102;
+    private int character1Id = 102;
+    private int character2Id = 34;
+    private int character3Id = 56;
+    private int character4Id = 79;
+    private int character5Id = 203;
 
+    private int[] characterIds = new int[5] ;
     private string APIUrl = "https://my-json-server.typicode.com/andruAJ/EntregaRestAPI";
     private string RicKandMortyUrl = "https://rickandmortyapi.com/api/character/";
 
     private UIDocument uiDocument;
 
-    private VisualElement rawImage;
-    private string name;
-    private string species;
+    private VisualElement imageChar1;
+    private string nameChar1;
+    private string speciesChar1;
 
     void Start()
     {
         uiDocument = GetComponent<UIDocument>();
         StartCoroutine(GetUser(1));
-        StartCoroutine(GetCharacter(characterId));  
-        rawImage = uiDocument.rootVisualElement.Q("CharacterPic") as VisualElement;
-    }
+        StartCoroutine(GetCharacter(characterIds));
+        imageChar1 = uiDocument.rootVisualElement.Q("CharacterPic") as VisualElement;
+        characterIds[character1Id, character2Id, character3Id, character4Id, character5Id];
 
     IEnumerator GetUser(int userId)
     {
@@ -53,32 +58,35 @@ public class HTTP_Test : MonoBehaviour
 
         }
     }
-    IEnumerator GetCharacter(int characterId)
+    IEnumerator GetCharacter(int[] characterIds)
     {
-        UnityWebRequest request = UnityWebRequest.Get(RicKandMortyUrl + characterId);
-        yield return request.SendWebRequest();
+        foreach (int id in characterIds)
+        {
+            UnityWebRequest request = UnityWebRequest.Get(RicKandMortyUrl + id);
+            yield return request.SendWebRequest();
 
-        if (request.result == UnityWebRequest.Result.ConnectionError)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            if (request.responseCode == 200)
+            if (request.result == UnityWebRequest.Result.ConnectionError)
             {
-                // Show results as text
-                string json = request.downloadHandler.text;
-                Character character = JsonUtility.FromJson<Character>(json);
-                Debug.Log(character.id + ":" + character.name + " is an " + character.species);
-                StartCoroutine(GetImage(character.image));
+                Debug.Log(request.error);
             }
             else
             {
-                string mensaje = "status:" + request.responseCode;
-                mensaje += "\nError: " + request.error;
-                Debug.Log(mensaje);
-            }
+                if (request.responseCode == 200)
+                {
+                    // Show results as text
+                    string json = request.downloadHandler.text;
+                    Character character = JsonUtility.FromJson<Character>(json);
+                    Debug.Log(character.id + ":" + character.name + " is an " + character.species);
 
+                    StartCoroutine(GetImage(character.image));
+                }
+                else
+                {
+                    string mensaje = "status:" + request.responseCode;
+                    mensaje += "\nError: " + request.error;
+                    Debug.Log(mensaje);
+                }
+            }
         }
     }
 
@@ -97,7 +105,7 @@ public class HTTP_Test : MonoBehaviour
                 // Show results as texture
                 Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
                 Debug.Log("Image downloaded successfully");
-                rawImage.style.backgroundImage = texture;
+                imageChar1.style.backgroundImage = texture;
             }
             else
             {
